1f31861bcd43b822d90e8f355b69d03310777102
Svusuperman <cody.henrichsen@740s-pl.local>
2016-07-27 12:43:02 -0600

    wrote all 3 get methods

--- SummerNodeProject/SummerArray.cpp
+++ SummerNodeProject/SummerArray.cpp
@@ -45,3 +45,15 @@ Type SummerArray<Type> :: getFromIndex(int index)
     
     return retrievedValue;
 }
+
+template <class Type>
+Type SummerArray<Type> :: getFirst()
+{
+    return front->getNodeData();
+}
+
+template <class Type>
+Type SummerArray<Type> :: getLast()
+{
+    return end->getNodeData();
+}
