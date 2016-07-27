47f1b0d967c296dc0a044da1f24803caa79a79f2
Svusuperman <cody.henrichsen@740s-pl.local>
2016-07-27 10:29:42 -0600

    New Nodes

---
+++ SummerNodeProject/SummerArray.cpp
@@ -0,0 +1,27 @@
+//
+//  SummerArray.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/27/16.
+//  Copyright © 2016 Henrichsen, Cody. All rights reserved.
+//
+
+#include "SummerArray.hpp"
+
+template <class Type>
+SummerArray<Type> :: SummerArray(int length)
+{
+    this->length = length;
+    modifiedNodes = new bool[length];
+    front = nullptr;
+    end = nullptr;
+    
+    DataNode<Type> * start = new DataNode<Type>();
+    front = start;
+    for(int index = 1; index < length; index++)
+    {
+        DataNode<Type> * next = new DataNode<Type>();
+        end->setNodePointer(next);
+        end = next;
+    }
+}
---
+++ SummerNodeProject/SummerArray.hpp
@@ -0,0 +1,36 @@
+//
+//  SummerArray.hpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/27/16.
+//  Copyright © 2016 Henrichsen, Cody. All rights reserved.
+//
+
+#ifndef SummerArray_hpp
+#define SummerArray_hpp
+
+#include <stdio.h>
+#include "DataNode.h"
+
+template <class Type>
+class SummerArray
+{
+private:
+    int length;
+    bool * modifiedNodes;
+    DataNode<Type> front;
+    DataNode<Type> end;
+public:
+    SummerArray(int length);
+    
+    Type getFromIndex(int index);
+    Type getFirst();
+    Type getLast();
+    void setAtIndex(int index, Type data);
+    
+    void sort();
+    void resize(int newSize);
+    int getUnusedNodes();
+};
+
+#endif /* SummerArray_hpp */
